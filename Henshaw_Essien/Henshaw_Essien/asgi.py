1c1607dfc1841f3c9760e9ca1361e5e1c0e982bf
josephhensh <josephhensh2000@gmail.com>
2022-06-20 09:25:56 +0100

    Initial commit: Create project (my Zuri username), manage migrations

---
+++ Henshaw_Essien/Henshaw_Essien/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Henshaw_Essien project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Henshaw_Essien.settings')
+
+application = get_asgi_application()
